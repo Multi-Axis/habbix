2b54bf45844be4f0919f4b93e1130e02007e92c0
Samuli Thomasson <simsaladin@paivola.fi>
2014-12-05 14:54:35 +0200

    change treshold db entry

--- src/Models.hs
+++ src/Models.hs
@@ -121,8 +121,9 @@ Future
 -- like 'y = threshold' lines in graphs
 Threshold
     item        ItemFutureId    sql=itemid
-    lower       Bool
-    value       Rational        sqltype=numeric(20,4)
+    normal      Rational        sqltype=numeric(20,4)
+    warning     Double    -- trigger if warning  * normal > value
+    critical    Double    -- trigger if critical * normal > value
     deriving Show
 
 -- attach pretty names (cpu, mem) to zabbix items
